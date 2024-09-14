2c2786ac0c852eedb3e9dea1a6e8b8be5de4533d
nachiket-kumar <nachi12234@gmail.com>
2024-09-14 10:15:23 +0530

    commiting python

---
+++ runPython.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+
+exec('python hello.py', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Output: ${stdout}`);
+});
+``
